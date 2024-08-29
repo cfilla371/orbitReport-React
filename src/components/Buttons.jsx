aea2811aaf46ba160d7f2601625b1d1f91d6c090
cfilla371 <64501150+cfilla371@users.noreply.github.com>
2024-08-29 15:19:57 -0500

    Commit message

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,25 @@
+import satData from './satData';
+import filterByType from '../App'
+import setSat from '../App'
+import displaySats from '../App'
+
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+      
+        return (
+          <button onClick={() => filterByType(sat)} key={id} >
+            {sat} Orbit
+          </button>
+        )
+      })}
+      
+      <button onClick={() => setSat(satData)}>
+        All Orbits
+      </button>
     </div>
   );
 };
